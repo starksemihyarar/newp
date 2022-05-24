b9e6d901e654418b52fce25d4b4dfc830ff1b9ef
burakcelen <burakcelen@starkajans.com>
2022-05-24 10:43:02 +0300

    asd

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,103 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav>
+    <h1>Links</h1>
+    <div>
+      <ul>
+        <li v-for="link in links" :key="link.id">
+          <a :href="link.url" v-if="!link.isHighlight">{{ link.name }}</a>
+          <a :href="link.url" v-else :class="{ highlight: !isHighlight }">{{
+            link.name
+          }}</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+  <form @submit.prevent>
+    <label for=" name">Name</label>
+    <input type="text" id="name" v-model="link.name" />
+
+    <label for="url">Url:</label>
+    <input type="text" id="url" v-model="link.url" />
+
+    <label for="radio1">Highlight ?</label>
+    <input
+      type="radio"
+      id="radio1"
+      :value="true"
+      name="highlight"
+      v-model="link.isHighlight"
+    />
+
+    <label for="radio2">No Highlight ?</label>
+    <input
+      type="radio"
+      id="radio2"
+      :value="false"
+      name="highlight"
+      v-model="link.isHighlight"
+    />
+    <label for="id">id</label>
+    <input type="number" id="id" v-model="link.id" />
+    <button @click="toggle">Sırala</button>
+    <button @click="sendfunc">Submit</button>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      links: [],
+      link: {
+        id: "",
+        name: "",
+        url: "",
+        isHighlight: false,
+      },
+    };
+  },
+  methods: {
+    sendfunc() {
+      this.links.push({ ...this.link });
+    },
+    toggle() {
+      if (!this.isActive) {
+        this.links.sort((a, b) => a.id - b.id);
+        this.isActive = true;
+      } else {
+        this.links.sort((a, b) => b.id - a.id);
+        this.isActive = false;
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+input {
+  display: block;
+  padding: 0.5rem 0.7rem;
+  margin: 1rem;
+  width: 70%;
+}
+form {
+  display: grid;
+  align-items: center;
+  max-width: 30%;
+}
+.highlight {
+  color: "blue";
+  font-size: "25px";
+  text-decoration: "none";
+}
+button {
+  padding: 1rem 0;
+  margin-bottom: 1rem;
+  max-width: 50%;
+  font-weight: 700;
+}
+ul {
+  list-style: none;
 }
 </style>
